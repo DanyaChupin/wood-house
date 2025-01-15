e8a10991ff68b8c21fbc6d66826725810f06dbe2
DanyaChupin <chupa.chupa.14@mail.ru>
2025-01-15 12:43:11 +0300

    [+] adaptive reviews

--- src/components/reviews-block/ReviewsBlock.tsx
+++ src/components/reviews-block/ReviewsBlock.tsx
@@ -9,9 +9,9 @@ import reviewsImg6 from '../../shared/images/reviews-6.png'
 
 export function ReviewsBlock() {
 	return (
-		<div className="overflow-x-scroll scrollbar-hide flex w-[calc(100%+30px)] lg:w-[calc(100%+60px)] ml-[-15px] lg:ml-[-30px] 2xl:w-full 2xl:ml-0 mb-[150px] lg:mb-[180px] 2xl:mb-[200px]">
-			<div className="w-[100%] ml-[15px] lg:ml-[30px] 2xl:ml-0 flex gap-[20px]">
-				<div className="flex w-[312px] shrink-0 flex-col gap-[20px]">
+		<div className="overflow-x-scroll lg:overflow-x-hidden scrollbar-hide flex w-[calc(100%+30px)] lg:w-full ml-[-15px] lg:ml-0 mb-[150px] lg:mb-[180px] 2xl:mb-[200px]">
+			<div className="w-full ml-[15px] lg:ml-[30px] 2xl:ml-0 flex lg:justify-between gap-x-[20px]">
+				<div className="flex w-full max-w-[312px] shrink-0 lg:shrink flex-col gap-[20px]">
 					<ImageBlock
 						imgUrl={reviewsImg1}
 						className="w-full h-[371px]"
@@ -31,7 +31,7 @@ export function ReviewsBlock() {
 						</div>
 					</div>
 				</div>
-				<div className="w-[312px] shrink-0 flex flex-col gap-[20px]">
+				<div className="w-full max-w-[312px] shrink-0 flex lg:shrink flex-col gap-[20px]">
 					<ImageBlock
 						imgUrl={reviewsImg2}
 						className="w-full h-[200px]"
@@ -43,8 +43,8 @@ export function ReviewsBlock() {
 						alt="one"
 					/>
 				</div>
-				<div className="flex flex-col">
-					<div className="flex w-[312px] shrink-0 flex-col gap-[20px] mt-[100px]">
+				<div className="flex flex-col w-full max-w-[312px]">
+					<div className="flex lg:w-full w-[312px] lg:max-w-[312px] shrink-0 lg:shrink flex-col gap-[20px] mt-[100px]">
 						<ImageBlock
 							imgUrl={reviewsImg4}
 							className="w-full h-[200px]"
@@ -58,7 +58,7 @@ export function ReviewsBlock() {
 									className="w-full h-full rounded-[50%]"
 								/>
 							</div>
-							<div className="flex flex-col shrink-0">
+							<div className="flex flex-col">
 								<p className="font-semibold">Артем</p>
 								<span className="text-GRAY_400 text-[12px]">
 									24.09.2024
@@ -66,7 +66,7 @@ export function ReviewsBlock() {
 							</div>
 						</div>
 					</div>
-					<div className="flex w-[312px] shrink-0 flex-col gap-[20px] mt-[100px]">
+					<div className="flex max-w-[312px] w-full shrink-0 lg:shrink flex-col gap-[20px] mt-[100px]">
 						<ImageBlock
 							imgUrl={reviewsImg5}
 							className="w-full h-[326px]"
@@ -85,7 +85,7 @@ export function ReviewsBlock() {
 						</div>
 					</div>
 				</div>
-				<div className="flex w-[312px] shrink-0 flex-col gap-[20px] pr-[15px] lg:pr-[30px] 2xl:pr-0">
+				<div className="flex w-full max-w-[312px] shrink-0 lg:shrink flex-col gap-[20px] pr-[15px] lg:pr-[30px] 2xl:pr-0">
 					<ImageBlock
 						imgUrl={reviewsImg6}
 						className="w-full h-[326px]"
