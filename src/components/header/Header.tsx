f312590b308c40a0999241355ad0affa4fbd868e
DanyaChupin <chupa.chupa.14@mail.ru>
2025-01-16 01:56:03 +0300

    [+] add header anim on load

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -77,7 +77,7 @@ export function Header() {
 		}
 	}
 	return (
-		<header className="h-[64px] sm:h-[68px] lg:h-[74px] 2xl:h-[80px] flex items-center relative mb-[70px] lg:mb-[111px] md:animate-translateXTop md:ease-out md:transition-[transform, opacity]">
+		<header className="h-[64px] sm:h-[68px] lg:h-[74px] 2xl:h-[80px] flex items-center relative mb-[70px] lg:mb-[111px] md:animate-fadeInBlur md:transition-opcaity">
 			<div className="flex items-center justify-between w-full px-[15px] lg:px-[25px] 2xl:px-[50px] overflow-x-hidden">
 				<CompanyLogo />
 				{!isMobile && <NavMenu onClick={toggleBurgerMenu} />}
