6fe5547bc8e7662779660e162544d2de2f10911a
DanyaChupin <chupa.chupa.14@mail.ru>
2025-01-17 16:55:56 +0300

    [+] add bron

--- src/screens/BronScreen.tsx
+++ src/screens/BronScreen.tsx
@@ -70,17 +70,9 @@ export function BronScreen() {
 			id="sliderScreen"
 			className="mb-[150px] lg:mb-[180px] 2xl:mb-[200px] transition-[transform, opacity] closeLeft duration-500"
 		>
-			<h1 className="flex justify-center w-full text-[72px]">
-				BronScreen
+			<h1 className="text-[64px] transition-transform sm:text-[72px]  w-full lg:text-[88px] leading-[72px] 2xl:leading-[112px] 2xl:text-[96px] flex justify-center mb-[71px] sm:mb-[61px] lg:mb-[50px]">
+				Бронирование
 			</h1>
-			<div className="flex w-full gap-[20px]">
-				<div className="w-[80%] h-[500px] bg-black" />
-				<div className="w-[20%] bg-black" />
-			</div>
-			<div className="flex w-full gap-[20px] mt-[20px]">
-				<div className="w-[60%] h-[300px] bg-black" />
-				<div className="w-[40%] bg-black" />
-			</div>
 
 			<div className="left" id="_bn_widget_" />
 		</main>
