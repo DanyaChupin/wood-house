81eff124d86e8a2d5e7382a644019b2b4122545e
DanyaChupin <chupa.chupa.14@mail.ru>
2025-01-17 17:19:05 +0300

    [~] fix title bron

--- src/screens/BronScreen.tsx
+++ src/screens/BronScreen.tsx
@@ -34,7 +34,7 @@ export function BronScreen() {
 			id="sliderScreen"
 			className="mb-[150px] lg:mb-[180px] 2xl:mb-[200px] transition-[transform, opacity] closeLeft duration-500"
 		>
-			<h1 className="text-[64px] transition-transform sm:text-[72px]  w-full lg:text-[88px] leading-[72px] 2xl:leading-[112px] 2xl:text-[96px] flex justify-center mb-[71px] sm:mb-[61px] lg:mb-[50px]">
+			<h1 className="text-[53px] transition-transform sm:text-[72px]  w-full lg:text-[88px] leading-[72px] 2xl:leading-[112px] 2xl:text-[96px] flex justify-center mb-[71px] sm:mb-[61px] lg:mb-[50px]">
 				Бронирование
 			</h1>
 			<div className="left" id="booking_iframe" />
