46ecb3c58def9d49efecff0639577066da3c06bb
DanyaChupin <chupa.chupa.14@mail.ru>
2025-01-22 11:20:56 +0300

    [~] change yandex link

--- src/components/home/YandexInfo.tsx
+++ src/components/home/YandexInfo.tsx
@@ -1,16 +1,9 @@
 'use client'
 
+import Link from 'next/link'
 import { ArrowIcon } from '@/shared/icons/Arrow-icon'
 
 export function YandexInfo() {
-	const moveToTop = () => {
-		const section = document.querySelector(`#yandex-map`)
-		if (section)
-			section.scrollIntoView({
-				behavior: 'smooth',
-				block: 'center',
-			})
-	}
 	return (
 		<div className="flex flex-col mb-[37px] lg:mb-[43px] 2xl:mb-[50px] items-start ml-[10%] sm:ml-[18%] lg:ml-[21%] gap-[30px] lg:gap-[23px] 2xl:gap-[15px]">
 			<h4 className="text-[32px] lg:text-[40px] leading-[40px] 2xl:leading-[56px] 2xl:text-[48px]">
@@ -22,13 +15,14 @@ export function YandexInfo() {
 				<br />
 				Поделитесь и вы
 			</h4>
-			<button
-				onClick={moveToTop}
-				className="py-[14px] bg-black font-[SFPro] hover:bg-GRAY_500 transition-[background-color] duration-200 font-bold rounded-[12px] text-white text-[16px] flex items-center gap-[8px] justify-center w-[202px]"
+			<Link
+				target="_blank"
+				href="https://yandex.ru/navi/org/vudxauc/143261160280?si=tn0mmygpnev860dyen4z7z1f0w"
+				className="py-[14px] bg-black font-[SFPro] hover:bg-GRAY_500 transition-[background-color] duration-200 font-bold rounded-[12px] text-white text-[16px] flex items-center gap-[8px] justify-center w-[202px] active:bg-GRAY_600"
 			>
 				Яндекс Карты
 				<ArrowIcon />
-			</button>
+			</Link>
 		</div>
 	)
 }
